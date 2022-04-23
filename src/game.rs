64f4c6f5d85ec0cb8cb6f1e59b68e13ae6be1703
cybersoulK <andre.explore.fit@gmail.com>
2022-04-22 18:23:35 -0700

    level 3

--- src/game.rs
+++ src/game.rs
@@ -1,11 +1,8 @@
-use pixel_graphics::GameLoop;
-use pixel_graphics::Engine;
+use pixel_graphics::*;
 
-use pixel_graphics::Model;
 
-use pixel_graphics::Camera;
-use pixel_graphics::DrawableObject;
-use pixel_graphics::Transform;
+mod cube;
+mod shader1;
 
 pub struct Game {}
 
@@ -15,22 +12,23 @@ impl GameLoop for Game {
     fn init(&mut self, engine: &mut Engine){
 
         let camera_transform = Transform::default();
-        
-        let main_camera = Camera::new(camera_transform, 1.0, 1000.0, 90.0);
+        engine.set_camera(camera_transform, 1.0, 1000.0, 90.0);
 
-        //engine.addObject(main_camera);
-        //engine.setCamera(main_camera);
 
+        let cube_mesh = cube::get_cube();
+        let cube_transform = Transform::default();
+
+        let cube_material = Material::new(Vec::);
+
+        let cube_model = Model::new(Vec::new(cube_mesh), Vec::new(cube_material));
+        let cube = DrawableObject::new(cube_transform, model);
+
+        engine.add_object(Rc::new(cube));
 
         let drawable_transform = Transform { 
             position: glam::vec3(100.0, 100.0, 100.0), 
             ..Default::default() 
         };
-      
-        //let drawable_model = engine.assets.load_model("asds/asdasd.tf");
-        //let drawable_object = DrawableObject::new(drawable_transform, drawable_model);
-        
-        //engine.addObject(drawable_object);
     }
 
     fn update(&mut self, engine: &mut Engine){
