9483d9023f6d80e7d5fad798507681d7e2afa436
cybersoulK <andre.explore.fit@gmail.com>
2022-04-28 02:46:07 -0700

    colorful wave

--- src/game/shaders/shader_plane.rs
+++ src/game/shaders/shader_plane.rs
@@ -1,4 +1,4 @@
-use std::rc::Rc;
+use std::{rc::Rc, f32::consts::PI};
 
 use glam::{Vec3, Vec4};
 
@@ -22,16 +22,26 @@ impl Shader for CustomShader {
         
         face_id %= 12;
 
-        core.color = match face_id {
+        /*core.color = match face_id {
             0 | 1 => Vec4::new(0.71, 0.0, 0.0, 1.0),
             2 | 3 => Vec4::new(0.0, 0.0, 0.78, 1.0),
             4 | 5 => Vec4::new(0.71, 0.71, 0.71, 1.0),
             6 | 7 => Vec4::new(0.0, 0.78, 0.78, 1.0),
             8 | 9 => Vec4::new(0.78, 0.0, 0.78, 1.0),
             _ => Vec4::new(0.71, 0.71, 0.0, 1.0),
-        };
+        };*/
 
         //core.color = Vec4::new(0.5294, 0.807843, 0.9216, 1.0);
+
+
+        core.color.x = (((core.vertex.x - 0.5).powf(2.0) + (core.vertex.z - 0.5).powf(2.0)).sqrt()
+                        * params.elapsed_time.as_secs_f32() * FREQUENCY).sin() * 0.6;
+
+        core.color.y = (((core.vertex.x - 0.5).powf(2.0) + (core.vertex.z - 0.5).powf(2.0)).sqrt()
+                        * params.elapsed_time.as_secs_f32() * FREQUENCY + PI * 2.0 / 3.0 * 1.0).sin() * 0.6;
+
+        core.color.z = (((core.vertex.x - 0.5).powf(2.0) + (core.vertex.z - 0.5).powf(2.0)).sqrt()
+                        * params.elapsed_time.as_secs_f32() * FREQUENCY + PI * 2.0 / 3.0 * 2.0).sin() * 0.6;
 
 
         const FREQUENCY: f32 = 4.0;
@@ -41,7 +51,7 @@ impl Shader for CustomShader {
                         * params.elapsed_time.as_secs_f32() * FREQUENCY).sin() * HEIGHT;
 
         core.color *= (0.2 * core.vertex.y / HEIGHT) + 0.8;
-
+        
         core
     }
 
